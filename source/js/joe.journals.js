b4f32b2ec7a1e5ec559e16e72156f78e79518272
qinhua <352484005@qq.com>
2021-11-10 00:21:58 +0800

    修改eslint配置

--- source/js/joe.journals.js
+++ source/js/joe.journals.js
@@ -1,203 +1,203 @@
 /**文章页逻辑 */
 const journalContext = {
-  /* 激活列表特效 */
-  initEffect() {
-    if (!ThemeConfig.enable_journal_effect) return;
-    new WOW({
-      boxClass: "wow",
-      animateClass: "fadeIn",
-      offset: 0,
-      mobile: true,
-      live: true,
-    }).init();
-  },
-  /* 点赞 */
-  initLike() {
-    if (!ThemeConfig.enable_like_journal) return;
-    const $allItems = $(".joe_journal__item");
-    if ($allItems.length) {
-      $allItems.each(function (_, item) {
-        const $this = $(this);
-        const cid = $this.attr("data-cid");
-        const clikes = +($this.attr("data-clikes") || 0);
-        let agreeArr = localStorage.getItem(encryption("agree-journal"))
-          ? JSON.parse(
-              decrypt(localStorage.getItem(encryption("agree-journal")))
-            )
-          : [];
-        const $iconLike = $this.find(".journal-like");
-        const $iconUnlike = $this.find(".journal-unlike");
-        const $likeNum = $this.find(".journal-likes-num");
-        if (agreeArr.includes(cid)) {
-          $iconLike.hide();
-          $iconUnlike.show();
-        } else {
-          $iconLike.show();
-          $iconUnlike.hide();
-        }
-        $likeNum.html(clikes);
-        $iconLike.on("click", function () {
-          let _loading = false;
-          if (_loading) return;
-          _loading = true;
-          agreeArr = localStorage.getItem(encryption("agree-journal"))
-            ? JSON.parse(
-                decrypt(localStorage.getItem(encryption("agree-journal")))
-              )
-            : [];
-          let flag = agreeArr.includes(cid);
-          $.ajax({
-            url: "/api/content/journals/" + cid + "/likes",
-            type: "POST",
-            dataType: "json",
-            data: {
-              type: flag ? "disagree" : "agree",
-            },
-            success(res) {
-              if (res.status !== 200) {
-                return;
-              }
-              let likes = clikes;
-              if (flag) {
-                likes--;
-                const index = agreeArr.findIndex((_) => _ === cid);
-                agreeArr.splice(index, 1);
-                $iconLike.show();
-                $iconUnlike.hide();
-              } else {
-                likes++;
-                agreeArr.push(cid);
-                $iconLike.hide();
-                $iconUnlike.show();
-              }
-              const name = encryption("agree-journal");
-              const val = encryption(JSON.stringify(agreeArr));
-              localStorage.setItem(name, val);
-              $likeNum.html(likes);
-            },
-            error(err) {
-              _loading = false;
-              return Qmsg.warning(err.responseJSON.message);
-            },
-            complete() {
-              _loading = false;
-            },
-          });
-        });
-      });
-    }
-  },
-  /* 评论及折叠 */
-  initComment() {
-    if (ThemeConfig.enable_clean_mode || !ThemeConfig.enable_comment_journal)
-      return;
-    $(".journal_comment_expander,.journal-comment").on("click", function () {
-      const $this = $(this);
-      const $parent = $this.parents(".footer-wrap");
-      const compComment = $parent.find("halo-comment")[0]._wrapper.$refs.inner;
-      // 展开加载评论
-      if (!$parent.hasClass("open")) {
-        compComment.loadComments();
-      }
-      $parent.toggleClass("open");
-      $parent
-        .find(".journal_comment_expander_txt")
-        .html(($parent.hasClass("open") ? "收起" : "查看") + "评论");
-    });
-  },
-  /* 内容折叠/展开 */
-  initExpander() {
-    $(".journal_content_expander i").on("click", function () {
-      $(this).parents(".joe_journal_body").toggleClass("open");
-    });
-  },
-  /* 复制 + 版权文字 */
-  // initCopy() {
-  //   $(".page-journals .joe_detail").on("copy", function (e) {
-  //     const body_element = document.body;
-  //     const selection = window.getSelection();
-  //     const appendLink = ThemeConfig.enable_copy_right_text
-  //       ? ThemeConfig.copy_right_text ||
-  //         `\r\n\r\n====================================<br>
-  //         作者：${ThemeConfig.author}<br>
-  //         来源：${ThemeConfig.blog_title}<br>
-  //         链接：${curl}<br>
-  //         版权声明：内容遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。`
-  //       : "";
-  //     if (window.clipboardData) {
-  //       const copytext = selection + appendLink;
-  //       window.clipboardData.setData("Text", copytext);
-  //       return false;
-  //     } else {
-  //       const copytext = selection + appendLink;
-  //       const newdiv = document.createElement("div");
-  //       newdiv.style.position = "absolute";
-  //       newdiv.style.left = "-99999px";
-  //       body_element.appendChild(newdiv);
-  //       newdiv.innerHTML = copytext;
-  //       selection.selectAllChildren(newdiv);
-  //       setTimeout(function () {
-  //         body_element.removeChild(newdiv);
-  //       }, 0);
-  //     }
-  //   });
-  // },
-  /* 初始化代码区域，高亮 + 折叠 + 复制 */
-  initCode() {
-    if (!$("pre[class*='language-']").length) return;
-    $("pre[class*='language-']").each(function (index, item) {
-      // 代码折叠
-      const $item = $(item);
-      const expander = $(
-        `<i class="joe-font joe-icon-arrow-downb code-expander" title="折叠/展开"></i>`
-      );
-      expander.on("click", function () {
-        const $parent = expander.parent("pre");
-        const $auto_fold = $parent.siblings(".toolbar").find(".autofold-tip");
-        $auto_fold && $auto_fold.remove();
-        expander.parent("pre").toggleClass("close");
-      });
-      $item.prepend(expander);
-      // 代码复制
-      const text = $(item).find("code[class*='language-']").text();
-      const span = $(
-        `<span class="copy-button"><i class="joe-font joe-icon-copy" title="复制代码"></i></span>`
-      );
-      new ClipboardJS(span[0], {
-        // text: () => text + "\r\n\r\n" + ThemeConfig.copy_right_text,
-        text: () => text,
-      }).on("success", () => Qmsg.success("复制成功！"));
-      $(item).addClass("copyable").append(span);
-    });
-  },
-  /* 日志块超长折叠 */
-  foldBlock() {
-    const $allBlocks = $(".joe_journal_body .content-wrp");
-    $allBlocks.each(function () {
-      const $this = $(this);
-      if ($this[0].getBoundingClientRect().height > 270) {
-        $this.siblings(".journal_content_expander").show();
-      }
-    });
-  },
-  /* 日志发布时间格式化 */
-  formatTime() {
-    const $allJournalTime = $(".joe_journal-posttime");
-    $allJournalTime.each(function () {
-      const $this = $(this);
-      $this.html(Utils.timeAgo($this.text()));
-    });
-  },
+	/* 激活列表特效 */
+	initEffect() {
+		if (!ThemeConfig.enable_journal_effect) return;
+		new WOW({
+			boxClass: "wow",
+			animateClass: "fadeIn",
+			offset: 0,
+			mobile: true,
+			live: true,
+		}).init();
+	},
+	/* 点赞 */
+	initLike() {
+		if (!ThemeConfig.enable_like_journal) return;
+		const $allItems = $(".joe_journal__item");
+		if ($allItems.length) {
+			$allItems.each(function (_, item) {
+				const $this = $(this);
+				const cid = $this.attr("data-cid");
+				const clikes = +($this.attr("data-clikes") || 0);
+				let agreeArr = localStorage.getItem(encryption("agree-journal"))
+					? JSON.parse(
+						decrypt(localStorage.getItem(encryption("agree-journal")))
+					)
+					: [];
+				const $iconLike = $this.find(".journal-like");
+				const $iconUnlike = $this.find(".journal-unlike");
+				const $likeNum = $this.find(".journal-likes-num");
+				if (agreeArr.includes(cid)) {
+					$iconLike.hide();
+					$iconUnlike.show();
+				} else {
+					$iconLike.show();
+					$iconUnlike.hide();
+				}
+				$likeNum.html(clikes);
+				$iconLike.on("click", function () {
+					let _loading = false;
+					if (_loading) return;
+					_loading = true;
+					agreeArr = localStorage.getItem(encryption("agree-journal"))
+						? JSON.parse(
+							decrypt(localStorage.getItem(encryption("agree-journal")))
+						)
+						: [];
+					let flag = agreeArr.includes(cid);
+					$.ajax({
+						url: "/api/content/journals/" + cid + "/likes",
+						type: "POST",
+						dataType: "json",
+						data: {
+							type: flag ? "disagree" : "agree",
+						},
+						success(res) {
+							if (res.status !== 200) {
+								return;
+							}
+							let likes = clikes;
+							if (flag) {
+								likes--;
+								const index = agreeArr.findIndex((_) => _ === cid);
+								agreeArr.splice(index, 1);
+								$iconLike.show();
+								$iconUnlike.hide();
+							} else {
+								likes++;
+								agreeArr.push(cid);
+								$iconLike.hide();
+								$iconUnlike.show();
+							}
+							const name = encryption("agree-journal");
+							const val = encryption(JSON.stringify(agreeArr));
+							localStorage.setItem(name, val);
+							$likeNum.html(likes);
+						},
+						error(err) {
+							_loading = false;
+							return Qmsg.warning(err.responseJSON.message);
+						},
+						complete() {
+							_loading = false;
+						},
+					});
+				});
+			});
+		}
+	},
+	/* 评论及折叠 */
+	initComment() {
+		if (ThemeConfig.enable_clean_mode || !ThemeConfig.enable_comment_journal)
+			return;
+		$(".journal_comment_expander,.journal-comment").on("click", function () {
+			const $this = $(this);
+			const $parent = $this.parents(".footer-wrap");
+			const compComment = $parent.find("halo-comment")[0]._wrapper.$refs.inner;
+			// 展开加载评论
+			if (!$parent.hasClass("open")) {
+				compComment.loadComments();
+			}
+			$parent.toggleClass("open");
+			$parent
+				.find(".journal_comment_expander_txt")
+				.html(($parent.hasClass("open") ? "收起" : "查看") + "评论");
+		});
+	},
+	/* 内容折叠/展开 */
+	initExpander() {
+		$(".journal_content_expander i").on("click", function () {
+			$(this).parents(".joe_journal_body").toggleClass("open");
+		});
+	},
+	/* 复制 + 版权文字 */
+	// initCopy() {
+	//   $(".page-journals .joe_detail").on("copy", function (e) {
+	//     const body_element = document.body;
+	//     const selection = window.getSelection();
+	//     const appendLink = ThemeConfig.enable_copy_right_text
+	//       ? ThemeConfig.copy_right_text ||
+	//         `\r\n\r\n====================================<br>
+	//         作者：${ThemeConfig.author}<br>
+	//         来源：${ThemeConfig.blog_title}<br>
+	//         链接：${curl}<br>
+	//         版权声明：内容遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。`
+	//       : "";
+	//     if (window.clipboardData) {
+	//       const copytext = selection + appendLink;
+	//       window.clipboardData.setData("Text", copytext);
+	//       return false;
+	//     } else {
+	//       const copytext = selection + appendLink;
+	//       const newdiv = document.createElement("div");
+	//       newdiv.style.position = "absolute";
+	//       newdiv.style.left = "-99999px";
+	//       body_element.appendChild(newdiv);
+	//       newdiv.innerHTML = copytext;
+	//       selection.selectAllChildren(newdiv);
+	//       setTimeout(function () {
+	//         body_element.removeChild(newdiv);
+	//       }, 0);
+	//     }
+	//   });
+	// },
+	/* 初始化代码区域，高亮 + 折叠 + 复制 */
+	initCode() {
+		if (!$("pre[class*='language-']").length) return;
+		$("pre[class*='language-']").each(function (index, item) {
+			// 代码折叠
+			const $item = $(item);
+			const expander = $(
+				"<i class=\"joe-font joe-icon-arrow-downb code-expander\" title=\"折叠/展开\"></i>"
+			);
+			expander.on("click", function () {
+				const $parent = expander.parent("pre");
+				const $auto_fold = $parent.siblings(".toolbar").find(".autofold-tip");
+				$auto_fold && $auto_fold.remove();
+				expander.parent("pre").toggleClass("close");
+			});
+			$item.prepend(expander);
+			// 代码复制
+			const text = $(item).find("code[class*='language-']").text();
+			const span = $(
+				"<span class=\"copy-button\"><i class=\"joe-font joe-icon-copy\" title=\"复制代码\"></i></span>"
+			);
+			new ClipboardJS(span[0], {
+				// text: () => text + "\r\n\r\n" + ThemeConfig.copy_right_text,
+				text: () => text,
+			}).on("success", () => Qmsg.success("复制成功！"));
+			$(item).addClass("copyable").append(span);
+		});
+	},
+	/* 日志块超长折叠 */
+	foldBlock() {
+		const $allBlocks = $(".joe_journal_body .content-wrp");
+		$allBlocks.each(function () {
+			const $this = $(this);
+			if ($this[0].getBoundingClientRect().height > 270) {
+				$this.siblings(".journal_content_expander").show();
+			}
+		});
+	},
+	/* 日志发布时间格式化 */
+	formatTime() {
+		const $allJournalTime = $(".joe_journal-posttime");
+		$allJournalTime.each(function () {
+			const $this = $(this);
+			$this.html(Utils.timeAgo($this.text()));
+		});
+	},
 };
 
 !(function () {
-  const omits = ["formatTime", "foldBlock"];
-  document.addEventListener("DOMContentLoaded", function () {
-    Object.keys(journalContext).forEach(
-      (c) => !omits.includes(c) && journalContext[c]()
-    );
-  });
-  window.addEventListener("load", function () {
-    journalContext.foldBlock();
-  });
+	const omits = ["formatTime", "foldBlock"];
+	document.addEventListener("DOMContentLoaded", function () {
+		Object.keys(journalContext).forEach(
+			(c) => !omits.includes(c) && journalContext[c]()
+		);
+	});
+	window.addEventListener("load", function () {
+		journalContext.foldBlock();
+	});
 })();
--- source/js/joe.leaving.js
+++ source/js/joe.leaving.js
@@ -1,142 +1,142 @@
 /**留言页逻辑 */
 const leavingContext = {
-  /* 获取留言板数据 */
-  getData() {
-    const sheetId = $(".joe_detail__title").attr("data-sheetid");
-    Utils.request(`/api/content/sheets/${sheetId}/comments/top_view`, "GET")
-      .then((res) => {
-        const $leavingList = $(".joe_detail__leaving-list");
-        const $leavingNone = $(".joe_detail__leaving-none");
-        if (res.total) {
-          const str = res.content.reduce((sum, item) => {
-            return (sum += `<li class="item">
+	/* 获取留言板数据 */
+	getData() {
+		const sheetId = $(".joe_detail__title").attr("data-sheetid");
+		Utils.request(`/api/content/sheets/${sheetId}/comments/top_view`, "GET")
+			.then((res) => {
+				const $leavingList = $(".joe_detail__leaving-list");
+				const $leavingNone = $(".joe_detail__leaving-none");
+				if (res.total) {
+					const str = res.content.reduce((sum, item) => {
+						return (sum += `<li class="item">
             <div class="user">
                 <img class="avatar lazyload" src="${
-                  ThemeConfig.lazyload_avatar
-                }" data-src="${
-              item.authorUrl || item.avatar
-            }" alt="用户头像" onerror="this.src='${
-              ThemeConfig.comment_avatar_error
-            }'"/>
+							ThemeConfig.lazyload_avatar
+							}" data-src="${
+								item.authorUrl || item.avatar
+							}" alt="用户头像" onerror="this.src='${
+								ThemeConfig.comment_avatar_error
+							}'"/>
                 <div class="nickname">${item.author}</div>
                 <div class="date">${new Date(
-                  item.createTime
-                ).toLocaleDateString()}</div>
+								item.createTime
+							).toLocaleDateString()}</div>
             </div>
             <div class="wrapper">
                 <div class="content">${item.content}</div>
             </div>
         </li>`);
-          }, "");
-          $leavingList.html(str);
-          leavingContext.randomColor();
-          $leavingList.show();
-          $leavingNone.hide();
-        } else {
-          $leavingList.hide();
-          $leavingNone.show();
-        }
-      })
-      .catch((err) => {
-        $leavingList.hide();
-        $leavingNone.show();
-      });
-  },
-  /* 随机样式 */
-  randomColor() {
-    let _index = 100;
-    const colors = [
-      "#dcc10c",
-      "#299bec",
-      "#EA5455",
-      "#7367F0",
-      "#32CCBC",
-      "#F6416C",
-      "#28C76F",
-      "#9F44D3",
-      "#F55555",
-      "#736EFE",
-      "#E96D71",
-      "#DE4313",
-      "#D939CD",
-      "#4C83FF",
-      "#F072B6",
-      "#C346C2",
-      "#5961F9",
-      "#FD6585",
-      "#465EFB",
-      "#FFC600",
-      "#FA742B",
-      "#5151E5",
-      "#BB4E75",
-      "#e255cd",
-      "#63c549",
-      "#0ECDDE",
-      "#F067B4",
-      "#F067B4",
-      "#ff9a9e",
-      "#1dd5de",
-      "#4facfe",
-      "#f093fb",
-      "#6fa3ef",
-      "#bc99c4",
-      "#46c47c",
-      "#f9bb3c",
-      "#e8583d",
-      "#f68e5f",
-    ];
-    const random = (min, max) =>
-      Math.floor(Math.random() * (max - min + 1)) + min;
-    const maxWidth = $(".joe_detail__leaving-list").width();
-    const maxHeight = $(".joe_detail__leaving-list").height();
-    const radius1 = [
-      "20px 300px",
-      "20px 400px",
-      "20px 500px",
-      "30px 300px",
-      "30px 400px",
-      "30px 500px",
-      "40px 300px",
-      "40px 400px",
-      "40px 500px",
-    ];
-    const radius2 = [
-      "300px 20px",
-      "400px 20px",
-      "500px 20px",
-      "300px 30px",
-      "400px 30px",
-      "500px 30px",
-      "300px 40px",
-      "400px 40px",
-      "500px 40px",
-    ];
-    $(".joe_detail__leaving-list .item").each((index, item) => {
-      const zIndex = random(1, 99);
-      const background = colors[random(0, colors.length - 1)];
-      const width = Math.ceil($(item).width());
-      const height = Math.ceil($(item).height());
-      const top = random(0, maxHeight - height);
-      const left = random(0, maxWidth - width);
-      $(item).css({
-        display: "block",
-        zIndex,
-        background,
-        top,
-        left,
-        borderTopLeftRadius: radius2[random(0, radius2.length - 1)],
-        borderTopRightRadius: radius1[random(0, radius1.length - 1)],
-        borderBottomLeftRadius: radius1[random(0, radius1.length - 1)],
-        borderBottomRightRadius: radius1[random(0, radius1.length - 1)],
-      });
-      $(item).draggabilly({ containment: true });
-      $(item).on("dragStart", (e) => {
-        _index++;
-        $(item).css({ zIndex: _index });
-      });
-    });
-  },
+					}, "");
+					$leavingList.html(str);
+					leavingContext.randomColor();
+					$leavingList.show();
+					$leavingNone.hide();
+				} else {
+					$leavingList.hide();
+					$leavingNone.show();
+				}
+			})
+			.catch((err) => {
+				$leavingList.hide();
+				$leavingNone.show();
+			});
+	},
+	/* 随机样式 */
+	randomColor() {
+		let _index = 100;
+		const colors = [
+			"#dcc10c",
+			"#299bec",
+			"#EA5455",
+			"#7367F0",
+			"#32CCBC",
+			"#F6416C",
+			"#28C76F",
+			"#9F44D3",
+			"#F55555",
+			"#736EFE",
+			"#E96D71",
+			"#DE4313",
+			"#D939CD",
+			"#4C83FF",
+			"#F072B6",
+			"#C346C2",
+			"#5961F9",
+			"#FD6585",
+			"#465EFB",
+			"#FFC600",
+			"#FA742B",
+			"#5151E5",
+			"#BB4E75",
+			"#e255cd",
+			"#63c549",
+			"#0ECDDE",
+			"#F067B4",
+			"#F067B4",
+			"#ff9a9e",
+			"#1dd5de",
+			"#4facfe",
+			"#f093fb",
+			"#6fa3ef",
+			"#bc99c4",
+			"#46c47c",
+			"#f9bb3c",
+			"#e8583d",
+			"#f68e5f",
+		];
+		const random = (min, max) =>
+			Math.floor(Math.random() * (max - min + 1)) + min;
+		const maxWidth = $(".joe_detail__leaving-list").width();
+		const maxHeight = $(".joe_detail__leaving-list").height();
+		const radius1 = [
+			"20px 300px",
+			"20px 400px",
+			"20px 500px",
+			"30px 300px",
+			"30px 400px",
+			"30px 500px",
+			"40px 300px",
+			"40px 400px",
+			"40px 500px",
+		];
+		const radius2 = [
+			"300px 20px",
+			"400px 20px",
+			"500px 20px",
+			"300px 30px",
+			"400px 30px",
+			"500px 30px",
+			"300px 40px",
+			"400px 40px",
+			"500px 40px",
+		];
+		$(".joe_detail__leaving-list .item").each((index, item) => {
+			const zIndex = random(1, 99);
+			const background = colors[random(0, colors.length - 1)];
+			const width = Math.ceil($(item).width());
+			const height = Math.ceil($(item).height());
+			const top = random(0, maxHeight - height);
+			const left = random(0, maxWidth - width);
+			$(item).css({
+				display: "block",
+				zIndex,
+				background,
+				top,
+				left,
+				borderTopLeftRadius: radius2[random(0, radius2.length - 1)],
+				borderTopRightRadius: radius1[random(0, radius1.length - 1)],
+				borderBottomLeftRadius: radius1[random(0, radius1.length - 1)],
+				borderBottomRightRadius: radius1[random(0, radius1.length - 1)],
+			});
+			$(item).draggabilly({ containment: true });
+			$(item).on("dragStart", (e) => {
+				_index++;
+				$(item).css({ zIndex: _index });
+			});
+		});
+	},
 };
 document.addEventListener("DOMContentLoaded", () => {
-  leavingContext.getData();
+	leavingContext.getData();
 });
